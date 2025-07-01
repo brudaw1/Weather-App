8a3708a45d275effb268cd02e0e1dff6af552536
Brunel Dawood <bdawood@depaul.edu>
2025-07-01 12:58:17 -0500

    first commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -84,7 +84,7 @@ const Weather = () => {
 
         {weatherData?<>
         <img src={weatherData.icon} alt="" className='weather-icon'/>
-        <p className='temperature'>{weatherData.temperature}°c</p>
+        <p className='temperature'>{weatherData.temperature}°F</p>
         <p className='location'>{weatherData.location}</p>
         <div className="weather-data">
             <div className="col">
